d5dfdb36d873aff492326ff3bad21e39a94a8627
prathamesh-a <89336149+prathamesh-a@users.noreply.github.com>
2021-11-02 18:41:27 +0530

    Update util

---
+++ src/prathamesh/Main.java
@@ -0,0 +1,9 @@
+package prathamesh;
+
+import prathamesh.util.input;
+
+public class Main {
+    public static void main(String[] args){
+        input input = new input();
+    }
+}
---
+++ src/prathamesh/util/input.java
@@ -0,0 +1,44 @@
+package prathamesh.util;
+
+import java.util.Scanner;
+
+public class input {
+    public int length;
+    public boolean includeSymbols;
+    public boolean includeNumbers;
+    public boolean includeLowercase;
+    public boolean includeUppercase;
+    private final Scanner sc = new Scanner(System.in);
+
+    public input(){
+        print("Enter length of the password: ");
+        this.length = check();
+        print("Include symbols? (e.g. @#$%)");
+        this.includeSymbols = checkInput();
+        print("Include numbers? (e.g. 1234)");
+        this.includeNumbers = checkInput();
+        print("Include lowercase letters? (e.g. abcd)");
+        this.includeLowercase = checkInput();
+        print("Include uppercase letters? (e.g. ABCD)");
+        this.includeUppercase = checkInput();
+    }
+
+    private void print(String str){System.out.println(str);}
+    private boolean checkInput(){
+        String string = sc.next();
+        while (!string.equalsIgnoreCase("yes") && !string.equalsIgnoreCase("no")){
+            print("Invalid input! Type YES or NO");
+            string = sc.next();
+        }
+        return string.equalsIgnoreCase("yes");
+    }
+    private int check(){
+        int num;
+        while (!sc.hasNextInt()){
+            print("Invalid input! Only numeric values allowed");
+            sc.next();
+        }
+        num = sc.nextInt();
+        return num;
+    }
+}
